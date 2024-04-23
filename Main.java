696b35a6e47472b5080a82cc8f98f02064da710d
Gustavo <131564127+gustavosantt@users.noreply.github.com>
2024-04-23 20:11:21 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -90,7 +90,7 @@ public class Main {
             }
         }
         JOptionPane.showMessageDialog(null, "Se eu conseguir mover montanhas, se eu conseguir surfar um tsunami, " +
-                "se eu conseguir domar o sol, se eu conseguir fazer o mar virar sertão, e o sertão virar mar, " +
+                "se eu conseguir domar o sol, se eu conseguir fazer o mar virar sertão, \n e o sertão virar mar, " +
                 "se eu conseguir dizer o que eu nunca vou conseguir dizer, aí terá chegado o dia em que eu vou " +
                 "conseguir te eliminar com alegria. Com " + eliminado.getVotos() + " votos, é você quem sai " +
                 eliminado.getNome() + "!");
